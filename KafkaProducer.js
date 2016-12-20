f56d7003f27abc1b1bad58c1ff14a6fde3fab30c
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2016-12-20 22:18:23 +0530

    round robin for producer

--- KafkaProducer.js
+++ KafkaProducer.js
@@ -1,9 +1,11 @@
 var Promise = require('bluebird');
 var kafka = require('kafka-node'),
     Producer = kafka.Producer,
+   // HighLevelProducer = kafka.HighLevelProducer,
     KeyedMessage = kafka.KeyedMessage,
-    client = new kafka.Client('172.24.36:2181,172.24.1.189:2181','nodeproducer',{partitionerType: 2}),
-    producer = new Producer(client);
+    client = new kafka.Client('172.24.36:2181,172.24.1.189:2181','nodeproducer'),
+    producer = new Producer(client,{partitionerType: 2});
+    //producerHighLevel = new HighLevelProducer(client);
 
 producer.on('ready', function() {
     console.log('producer is runnign ')
@@ -16,6 +18,22 @@ producer.on('error', function(err) {
 });
 
 //need to parse user id for the partitioning 
+// function send_to_kafka_by_partition(data,partition) {
+
+//     return new Promise(function(resolve, reject) {
+//         console.log('data: '+JSON.stringify(data))
+//         console.log('partition :'+partition);
+//         payloads = [
+//             { topic: 'textmessages', messages:JSON.stringify(data),partition:partition}
+//         ];
+//         producer.send(payloads, function(err, data) {
+//             console.log(data);
+//             resolve(data);
+//         });
+
+//     })
+// }
+
 function send_to_kafka(data) {
 
     return new Promise(function(resolve, reject) {
@@ -32,6 +50,7 @@ function send_to_kafka(data) {
 
     })
 }
+
 
 module.exports = {
     send_to_kafka: send_to_kafka
--- routes/index.js
+++ routes/index.js
@@ -37,10 +37,28 @@ router.get('/cass/read/', function(req, res) {
 });
 
 
+// router.post('/kafka/send/part/:part', function(req, res) {
+    
+//     return new Promise(function(resolve, reject) {
+//         var partition = req.params.part;
+//         //console.log(req.body)
+//         kafkaBl.send_to_kafka_by_partition(req.body,partition)
+//             .then(function(result) {
+//                 res.json(result);
+//             })
+//             .catch(function(err) {
+//                 console.log(err)
+//                 res.status(400).send(err);
+//             })
+//     });
+// });
+
+
 router.post('/kafka/send/', function(req, res) {
+    
     return new Promise(function(resolve, reject) {
         //console.log(req.body)
-        kafkaBl.send_to_kafka(req.body)
+        kafkaBl.send_to_kafka_by_partition(req.body)
             .then(function(result) {
                 res.json(result);
             })
