1e5170f815aac68e3e050bdf7eff161b2965e3c6
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2017-01-03 17:54:53 +0530

    back to previous state

--- RabbitProducer.js
+++ RabbitProducer.js
@@ -20,7 +20,7 @@ amqp.connect('amqp://test:test@172.24.1.36', function(err, conn) {
 
 function send_to_rabbitmq(data) {
     return new Promise(function(resolve, reject) {
-
+        console.log(JSON.stringify(data));
         setTimeout(function() {
             channel.publish(ex, key, new Buffer(JSON.stringify(data)));
         }, 0)
