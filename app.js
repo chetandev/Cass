e0c04a5bc9e91af7c82fbcbced55e64c51fdbb44
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2017-01-04 22:20:45 +0530

    posix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ posix.setrlimit('nofile', { soft: 10000, hard: 999999 });
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
