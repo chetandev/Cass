318fd4f2e29eb122e9927eff2875b3489aa5fd6b
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2017-01-05 22:14:57 +0530

    with settimeout

--- cass.js
+++ cass.js
@@ -6,8 +6,8 @@ var client = new cassandra.Client({
     keyspace: 'messagemicroservice',
     pooling: {
         coreConnectionsPerHost: {
-            [distance.local]: 2,
-            [distance.remote]: 2
+            [distance.local]: 50,
+            [distance.remote]: 50
         }
     }
 });
