4cc5443922304e521fa403f7179513e68888b367
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2017-01-06 12:15:22 +0530

    simple loop

--- cass.js
+++ cass.js
@@ -23,6 +23,7 @@ function put_in_cass(data) {
             var queries = [];
             var obj = JSON.parse(data.join('')).messages;
             var length = obj.length;
+            resolve('done');
             // for (var i = 0; i < length; i++) { //loop to be improved later
 
             //     var params = [cassandra.types.Uuid.random(), '1234', obj[i].address, obj[i].body, obj[i].date, obj[i]._id, obj[i].type]
